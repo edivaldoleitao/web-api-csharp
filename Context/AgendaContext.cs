08f5245e2ee33da076b8b2b9af728a273b89e76b
Edivaldo Leitão <mandoritos.ea@gmail.com>
2024-03-06 21:25:49 -0300

    adicao do pacote do framework entity e criacao de entidade

---
+++ Context/AgendaContext.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Context
+{
+    public class AgendaContext : DbContext
+    {
+        //faz a conexão com o banco de dados com a seguinte config
+        public AgendaContext(DbContextOptions<AgendaContext> options) : base(options)
+        {
+            
+        }
+        //representa a tabela no banco de dados
+        public DbSet<Contato> Contatos{ get; set; }
+    }
+}
