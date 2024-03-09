c65139ab90d001c0a66ec4b31c1a08a3b9566541
Edivaldo Leitão <mandoritos.ea@gmail.com>
2024-03-09 18:34:44 -0300

    configurando conexão do banco de dados e atualizando readme

--- Program.cs
+++ Program.cs
@@ -1,6 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using API.Context;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+//configura contexto do SQL SERVER passando a conexão do appsettings
+builder.Services.AddDbContext<AgendaContext>(options => 
+            options.UseSqlServer(builder.Configuration.GetConnectionString("ConexaoPadrao")));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
